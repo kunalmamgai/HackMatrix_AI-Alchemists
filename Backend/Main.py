3ba127025cc3b308627bbfc9c739589c9a816327
rasesh13 <raseshvarshney@gmail.com>
2026-04-03 11:02:39 +0530

    Update Main.py

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+<<<<<<< HEAD
 from routes import device_routes, center_routes
+=======
+from Routes import Device_Routes, Center_Routes
+>>>>>>> 063a5b8e2a3c2b451ed52efdebb025942cfd6883
 
 app = FastAPI(title="E-Scrape Mart API")
 
-app.include_router(device_routes.router, prefix="/devices", tags=["Devices"])
-app.include_router(center_routes.router, prefix="/centers", tags=["Centers"])
+app.include_router(Device_Routes.router, prefix="/devices", tags=["Devices"])
+app.include_router(Center_Routes.router, prefix="/centers", tags=["Centers"])
 
 @app.get("/")
 def root():
